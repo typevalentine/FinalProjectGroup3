0eabcd5ee9c90bdc4c82325fab070f8aed934ef9
typevalentine <koilui7112@gmail.com>
2026-07-30 12:43:49 -0400

    Added Table

---
+++ Models/TeamMemberData.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalProjectGroup3.Models
+{
+    public class TeamMemberData
+    {
+        public string? FullName { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public string? CollegeProgram { get; set; }
+
+        public int YearInProgram { get; set; }
+
+        public string? Hobby { get; set; }
+    }
+}
+
+
