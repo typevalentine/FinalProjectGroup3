11ede921770661ac5454de68be9e312a3bdbee78
N0thaha <133283430+N0thaha@users.noreply.github.com>
2026-07-30 10:55:14 -0400

    Added 2 Tables

---
+++ Models/Cat.cs
@@ -0,0 +1,11 @@
+namespace FinalProjectGroup3.Models
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool HasAutoFeeder { get; set; }
+        public string FurColor { get; set; }
+    }
+}
