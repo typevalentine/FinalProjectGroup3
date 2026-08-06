1edb602288e0419a893036fe9d3bb523327a0d08
typevalentine <koilui7112@gmail.com>
2026-08-06 14:28:01 -0400

    Created controllers

---
+++ Controllers/CatController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProjectGroup3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatController : ControllerBase
+    {
+        private readonly ILogger<CatController> _logger;
+
+        public CatController(ILogger<CatController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        
+        public IActionResult Get()
+        {
+            var catData = new Models.Cat
+            {
+                Id = 2506,
+                Name = "Victoria",
+                Age = 10,
+                HasAutoFeeder = true,
+                FurColor = "Black"
+            };
+            return Ok(catData);
+        }
+    }
+}
