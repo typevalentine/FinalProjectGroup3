35715eb840c464de704d151f55458659e304f1cd
typevalentine <koilui7112@gmail.com>
2026-08-06 14:28:01 -0400

    Created controllers

---
+++ Controllers/TeamMemberDataController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProjectGroup3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeamMemberDataController : ControllerBase
+    {
+        private readonly ILogger<TeamMemberDataController> _logger;
+
+        public TeamMemberDataController(ILogger<TeamMemberDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var teamMemberData = new Models.TeamMemberData
+            {
+                FullName = "Valentine Miller",
+                Birthdate = new DateTime(2006, 1, 11),
+                CollegeProgram = "Information Technology",
+                YearInProgram = 3,
+                Hobby = "Drawing"
+            };
+            return Ok(teamMemberData);
+        }
+    }
+}
---
+++ Controllers/VideoGameController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProjectGroup3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoGameController : ControllerBase
+    {
+        private readonly ILogger<VideoGameController> _logger;
+
+        public VideoGameController(ILogger<VideoGameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var videoGameData = new Models.VideoGame
+            {
+                Id = 34821,
+                Title = "Outer Wilds",
+                Engine = "Unity",
+                ReleaseYear = 2019,
+                IsMultiplayer = false
+            };
+            return Ok(videoGameData);
+        }
+    }
+}
