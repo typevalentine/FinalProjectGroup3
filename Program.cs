6e658d5f05f10ec576da1d6e1ddafbd5628b6d5a
Samhita Madireddy <madireddysamhita@gmail.com>
2026-08-08 14:21:40 -0400

    Resolved merge conflicts, keep Azure connection setup

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
 
 var app = builder.Build();
 
