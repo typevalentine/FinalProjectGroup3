dc04ba96a63c5a47d84f5e01e96f0c470ee73465
typevalentine <koilui7112@gmail.com>
2026-08-08 08:36:46 -0400

    Added TravelDestination Controller

---
+++ Controllers/TravelDestinationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProjectGroup3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TravelDestinationController : ControllerBase
+    {
+        private readonly ILogger<TravelDestinationController> _logger;
+
+        public TravelDestinationController(ILogger<TravelDestinationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var travelDestinationData = new Models.TravelDestination
+            {
+                Id = 95432,
+                DestinationName = "Berlin",
+                Country = "Germany",
+                Description = "The capital of Germany, they are known for their nightlife and historical landmarks.",
+                BestSeasonToVisit = "Late Spring and Early Summer",
+                Rating = 5
+            };
+            return Ok(travelDestinationData);
+        }
+    }
+}
